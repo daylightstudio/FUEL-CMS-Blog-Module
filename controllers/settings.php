651523d148546f3b5f2ffef67f33932db3f6cb3a
Jonathan Lee <jon@specialbrand.net>
2012-04-12 18:20:40 -0700

    Updated to use fuel_settings

--- controllers/settings.php
+++ controllers/settings.php
@@ -1,57 +1,39 @@
 <?php
 require_once(FUEL_PATH.'/libraries/Fuel_base_controller.php');
 
-class Settings extends Fuel_base_controller {
+class Settings extends Fuel_base_controller
+{
 	public $view_location = 'blog';
 	public $nav_selected = 'blog/settings';
-	
+
 	function __construct()
 	{
 		parent::__construct();
 		$this->config->module_load('blog', 'blog');
 		$this->_validate_user('blog/settings');
 	}
-	
+
 	function index()
 	{
-		$this->load->module_model(FUEL_FOLDER, 'settings_model');
 		$this->js_controller_params['method'] = 'add_edit';
-		
-		if ( ! empty($_POST['settings']))
-		{
-			// clear out old settings
-			$this->settings_model->delete(array('module' => 'fuel_blog'));
-			// format data for saving
-			$save = array();
-			$settings = $this->input->post('settings', TRUE);
-			foreach ($settings as $key => $value)
-			{
-				$value = trim($value);
-				if (empty($value)) {
-					continue;
-				}
-				$save[] = array(
-					'module' => 'fuel_blog',
-					'key'    => $key,
-					'value'  => $value,
-					);
-			}
-			$this->fuel->blog->remove_cache();
-			$this->settings_model->save($save);
-			$this->session->set_flashdata('success', lang('data_saved'));
-			redirect($this->uri->uri_string());
-		}
-		
-		$field_values = $this->settings_model->options_list('fuel_settings.key', 'fuel_settings.value', array('module' => 'fuel_blog'), 'key');
-		
-		$this->load->library('form_builder');
 		
 		$blog_config = $this->config->item('blog');
 		$fields = $blog_config['settings'];
 		
-	//	$this->form_builder->id = 'form';
+		if ($this->fuel->settings->process('fuel_blog', $fields))
+		{
+			$this->fuel->blog->remove_cache();
+			$this->session->set_flashdata('success', lang('data_saved'));
+			redirect($this->uri->uri_string());
+		}
+		$field_values = $this->fuel->settings->get('fuel_blog');
+		$fields['title']['after_html'] = '&nbsp;&nbsp;<strong>' . fuel_settings('fuel_blog', 'title') . '</strong>';
+		
+		$this->load->library('form_builder');
+		
+		//$this->form_builder->id = 'form';
 		$this->form_builder->label_layout = 'left';
-		$this->form_builder->form->validator = &$this->settings_model->get_validation();
+		$this->form_builder->form->validator = $this->fuel->settings->get_validation();
 		//$this->form_builder->submit_value = null;
 		$this->form_builder->use_form_tag = FALSE;
 		$this->form_builder->set_fields($fields);
@@ -59,7 +41,6 @@ class Settings extends Fuel_base_controller {
 		$this->form_builder->name_array = 'settings';
 		$this->form_builder->submit_value = 'Save';
 		$this->form_builder->set_field_values($field_values);
-		
 		
 		$vars = array();
 		$vars['form'] = $this->form_builder->render();
@@ -69,7 +50,5 @@ class Settings extends Fuel_base_controller {
 		$this->fuel->admin->set_titlebar($crumbs, 'ico_blog_settings');
 		
 		$this->fuel->admin->render('_admin/settings', $vars, Fuel_admin::DISPLAY_NO_ACTION);
-
 	}
-
 }
