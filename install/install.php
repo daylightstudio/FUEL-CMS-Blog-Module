e08773580f905907f80bf954d6b15202f865c2cc
David McReynolds <dave@thedaylightstudio.com>
2012-06-13 10:13:32 -0700

    Added install file for blog

---
+++ install/install.php
@@ -0,0 +1,17 @@
+<?php 
+$config['name'] = 'Blog Module';
+$config['version'] = BLOG_VERSION;
+$config['author'] = 'David McReynolds';
+$config['company'] = 'Daylight Studio';
+$config['license'] = 'Apache 2';
+$config['copyright'] = '2012';
+$config['author_url'] = 'http://www.thedaylightstudio.com';
+$config['developer_url'] = 'http://www.thedaylightstudio.com';
+$config['description'] = 'The FUEL Blog Module can be used to create posts and allow comments in an organized manner on your site.';
+$config['compatibility'] = '1.0';
+$config['instructions'] = '';
+$config['permissions'] = 'blog';
+$config['migration_version'] = 1;
+$config['install_sql'] = 'fuel_blog.sql';
+$config['uninstall_sql'] = '';
+?>
