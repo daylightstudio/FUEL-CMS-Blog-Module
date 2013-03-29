a073e64a817335a29d9d78b80033e008d632434e
David McReynolds <dave@thedaylightstudio.com>
2013-03-29 14:04:45 -0700

    fix for issue #5 regarding csrf and the search block

--- views/themes/default/_blocks/search.php
+++ views/themes/default/_blocks/search.php
@@ -3,7 +3,13 @@
 		<input type="text" name="q" value="" id="q" class="fillin_input" />
 		<input type="submit" value="Search" class="search_btn" />
 		
-		<!-- dummy value. Used so that we can get query strings to work if form method equals GET (which by defualt it is not)... needs more then one query string param to work -->
+		<!-- dummy value. Used so that we can get query strings t00o work if form method equals GET (which by defualt it is not)... needs more then one query string param to work -->
 		<input type="hidden" name="x" value="" /> 
+		<?php
+		if ($this->config->item('csrf_protection')) :
+		    $this->security->csrf_set_cookie();
+		?>
+		    <input type="hidden" name="<?php echo $this->security->get_csrf_token_name();?>" value="<?php echo $this->security->get_csrf_hash();?>"/>
+		<?php endif;?>
 	</form>
 </div>
