411a6db9088e8b413040f6f610ceb7333c5d6e29
David McReynolds <dave@thedaylightstudio.com>
2013-11-25 09:03:13 -0800

    made some adjustments to the blog module SQL updates

--- install/upgrade/fuel_blog_1.0_schema_changes.sql
+++ install/upgrade/fuel_blog_1.0_schema_changes.sql
@@ -8,7 +8,15 @@ ALTER TABLE `fuel_blog_posts` ADD `list_image` VARCHAR(100)  NOT NULL  DEFAULT '
 ALTER TABLE `fuel_blog_posts` ADD `post_date` DATETIME  NOT NULL  AFTER `allow_comments`;
 INSERT INTO `fuel_blog_settings` (`name`, `value`) VALUES ('page_title_separator', '&laquo;');
 ALTER TABLE `fuel_blog_comments` CHANGE `author_ip` `author_ip` VARCHAR(50)  NOT NULL  DEFAULT '';
+ALTER TABLE `fuel_blog_users` ADD `linkedin` VARCHAR(255)  NOT NULL  DEFAULT ''  AFTER `facebook`;
 ALTER TABLE `fuel_blog_users` ADD `google` VARCHAR(255)  NOT NULL  DEFAULT ''  AFTER `linkedin`;
+
+ALTER TABLE `fuel_blog_users` CHANGE `fuel_user_id` `fuel_user_id` INT(10)  UNSIGNED  NOT NULL;
+ALTER TABLE `fuel_blog_users` DROP PRIMARY KEY;
+ALTER TABLE `fuel_blog_users` ADD `id` INT  UNSIGNED  NOT NULL  AUTO_INCREMENT  PRIMARY KEY FIRST;
+UPDATE `fuel_blog_users` SET `id` = `fuel_user_id`;
+
+
 
 # Migrate Fuel Blog Categories to Fuel Relationships
 
