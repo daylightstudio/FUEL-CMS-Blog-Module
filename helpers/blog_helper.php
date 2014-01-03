e6e237c70f51f08275b7b34607d610a53c42aedd
David McReynolds <dave@thedaylightstudio.com>
2014-01-03 13:53:19 -0800

    updated links to the documentation in the comments

--- helpers/blog_helper.php
+++ helpers/blog_helper.php
@@ -23,7 +23,7 @@
  * @subpackage	Helpers
  * @category	Helpers
  * @author		David McReynolds @ Daylight Studio
- * @link		http://www.getfuelcms.com/user_guide/modules/blog/blog_helper
+ * @link		http://docs.getfuelcms.com/modules/blog/blog_helper
  */
 
 
