9a24d37d75c45dad72707777d384d11189a97691
David McReynolds <dave@bydaylight.com>
2015-10-08 00:15:44 -0700

    Fix for array_dot function error

--- libraries/Stopforumspam.php
+++ libraries/Stopforumspam.php
@@ -214,7 +214,7 @@ class Stopforumspam extends Fuel_base_library {
 
 		if (isset($key))
 		{
-			return array_dot($key, $this->_result, $default);
+			return array_get($key, $this->_result, $default);
 		}
 		else
 		{
